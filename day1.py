5d71bf7b0695f7f001d9b0e50fdc3c16fcf68b5e
SaiSivaYashaswi <yashaswi1230@gmail.com>
2026-08-12 10:31:19 +0530

    Day 1: Python fundamentals and basic examples
    Day 1: Python fundamentals — introduction to programming, procedural and object-oriented programming, Python characteristics, dynamic typing, interpreter concepts, applications, history, and basic coding examples.

---
+++ day1.py
@@ -0,0 +1,58 @@
+'''What is the program ? 
+The program is the process of the instructions given by the user or the programmer to planning what to do and how to do. 
+
+user_en = 5000
+bal_ = 10000
+result = bal_ - user_en 
+print(result)
+
+Procedual language : An procedual language is organized mainly on functions 
+example : C(language)
+def cal_ad(a,b):
+  return a + b
+a= 7
+b = 8
+any_ = call_ad(a,b)
+print(any_)
+
+Objected Oriented Language system(oops) : An Objected Oriented Language system is organized mainly on classes and object
+example: 
+num = 7 
+print(type(num))
+
+what is python ?
+It is high-level language, Interpreter Language, Dynamically typed language, 
+high level language example :
+n = 7 
+print(type(n))
+print(id(n)) --> Python take care about memory allocation no need to worry about it.
+Interpreter language example:
+Line by line execution is followed by python that is the reason we call it as a python is a interpreter language. 
+num = 9
+num_1 = 90
+print(num)
+Print(num_1)--> here in the console the error will appear because in this line the error will be shown in the terminal.
+Dynamically Typed language example: No need to mention the type of data passing to the variable.
+num = 78
+num1 = ['Python']
+num2 = [1,2]
+num3 = (1,2)
+print(type(num))
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+Why python ?
+1.More library
+2.Cross-platform
+3.Open-source4
+4.Simple Syntax 
+
+Application : 
+use in Data science
+Web dev
+Ai/ML
+
+History of the python: It was started in early 1980's and released in the years 1991 by Guido Van Rossum
+And he picked name from the most loved series called 'Monty python'
+first version was released in the year 1991 which is python 0.9.0 and now the new version is 3.14.x '''
