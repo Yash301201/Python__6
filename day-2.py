f28e668868422758b21f24eb8bcad015322b110b
SaiSivaYashaswi <yashaswi1230@gmail.com>
2026-08-14 11:14:57 +0530

    Add Day 2 Python fundamentals
    Added Day 2 Python practice covering tokens, identifiers, keywords, literals, operators, statements, comments, variable naming rules, multiple assignment, and variable swapping.

---
+++ day-2.py
@@ -0,0 +1,41 @@
+'''
+Tokens 
+-------
+--> Tokens are the small unit in the python 
+1.Identifier : Identifier is the name given to the variable, function, class, module, etc.
+--------------
+variable --> 
+num = 7
+print(type(num))
+
+functions
+def add_(a,b):
+  print (a + b)
+add_(5, 3)
+
+class
+class details:
+    pass
+person_1 = details 
+
+2. Keywords: Keywords are the reserved words in Python that have special meanings and cannot be used as identifiers.
+
+example: if, else, for, while, def, class, etc.
+
+Literals: Literals are the fixed values assigned to variables or constants in Python.(Every datatypes are literals)
+example: 5, "Hello", True, None
+num = 7
+name = "Yash"
+
+4.Operators: Operators are special symbols that perform operations on operands.
+example: +, -, *, /, %, **, //, ==, !=, >, <, >=, <=, and, or, not
+a = 10
+
+statement: Statements are the instructions that the Python interpreter can execute.
+example: print("Hello, World!")
+
+Comments: Comments are used to explain the code and are ignored by the Python interpreter.
+example: # This is a comment
+single line comment --> # This is a single line comment
+multi-line comment --> """This is a multi-line comment"""
+'''
