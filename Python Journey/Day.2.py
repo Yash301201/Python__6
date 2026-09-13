23d433dec065fbfec5b01dac55b16afeabcccaeb
Yashaswi <yashaswi1230@gmail.com>
2026-09-13 21:31:28 +0530

    Learned Python variables, tokens, comments, and basic syntax

---
+++ Python Journey/Day.2.py
@@ -0,0 +1,111 @@
+'''
+Tokens 
+>Tokens are the small unit in the python.
+example:
+num = 'python'
+print(type(num))
+functions
+def add(a,b):
+    print(a+b)
+add_(4,5)
+
+Class
+>class details:
+     pass
+per_1 = details
+
+identifers
+> Is a name of varible or functio or class
+
+keywords
+>keywords already saved in python for an specified reason to run.
+exaples:
+if
+else
+for
+while
+return
+print
+
+Literals
+>literals are data types that need to be saved in varibles.
+examples:
+num = 90
+name = 'teja'
+
+Operator
++, -, =
+
+Statements
+>statements are the instructions give to the program.
+example
+num = 90
+ 
+age = 2
+if age >= 18:
+   print(age)
+
+Comments:
+>Once comments are open the lines inside will never execute in python file.
+1.Single line comments (#)
+> it is used to comment only one line
+example:
+age = 20
+if age >= 18: #this checks age is greater or equal
+   print(age)
+2.Multi line comments (''' ''', """ """)
+>Uses to comment more then one line
+example:
+''' 
+python
+
+''''
+Varibles rules
+Bad ways 
+>can't use number at 1st postion
+>can't use special char anywhere
+>can't use space
+>we can't use keywords
+examples:
+2num = 90
+$num = 89
+n um = 34
+if = 21
+
+Good ways:
+>Small letters, Capital letters or (_) under
+example:
+nUm = 90
+Num = 78
+teja _garikapathi = 90
+a = {'name':'teja',
+     'AC_num': '342576878'}
+b = {'name':'garikapati',
+     'AC_num'='64836826897'}
+c = {'name':'ampolu',
+    'AC_num' ='34267375679'}
+SBI_teja_details ={'name' = 'teja',
+      'AC_num'='3422725267'}
+ICIC_garikapati_details ={'name' = 'garikapati',
+      'AC_num'='23235776662'}
+
+>Assiging varibles:
+num = 90
+print(num)
+
+num_3, num_2 = 56,65
+print(num_3)
+print(num_2)
+
+>Swaping of varibles:
+a , b = 45, 67
+print('a=',a)
+print('b=',b)
+a, b = b, a
+print('a=',a)
+prnt('b=',b)
+'''
+
+
+
+
